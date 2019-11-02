98519aaa2e2169d7e80cf478a3cb6882c2738efb
Kevin <kevinsantos@gmail.com>
2019-11-01 21:34:06 -0300

    Primeiro Commit

---
+++ PadariaEMerceariaDaFah/PadariaEMerceariaDaFah/GerenciaEmpresa.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Arquivos;
+
+namespace Comercio
+{
+    [Serializable]
+    public class GerenciaEmpresa
+    {
+        public List<Funcionario> Funcionarios;
+        public List<Produto> Produtos;
+        public List<Cliente> Clientes;
+        public List<Venda> Vendas;
+
+        public static GerenciaEmpresa Instance;
+
+        public GerenciaEmpresa()
+        {
+            Funcionarios = new List<Funcionario>();
+            Produtos = new List<Produto>();
+            Clientes = new List<Cliente>();
+            Vendas = new List<Venda>();
+            Instance = this;
+        }
+
+        public void AdicionarFuncionario(Funcionario Novo)
+        {
+            Funcionarios.Add(Novo);
+        }
+        public void AdicionarProduto(Produto Novo)
+        {
+            Produtos.Add(Novo);
+        }
+        public void AdicionarCliente(Cliente Novo)
+        {
+            Clientes.Add(Novo);
+        }
+        public void AdicionarVenda(Venda Novo)
+        {
+            Vendas.Add(Novo);
+        }
+
+        public void RemoverFuncionario(Funcionario Item)
+        {
+            Funcionarios.Remove(Funcionarios.Find(x => x.Codigo == Item.Codigo));
+        }
+        public void RemoverProduto(Produto Item)
+        {
+            Produtos.Remove(Produtos.Find(x=> x.Codigo == Item.Codigo)) ;
+        }
+        public void RemoverCliente(Cliente Item)
+        {
+            Clientes.Remove(Clientes.Find(x => x.Codigo == Item.Codigo));
+        }
+        public void RemoverVenda(Venda Item)
+        {
+            Vendas.Remove(Vendas.Find(x => x.Codigo == Item.Codigo));
+        }
+
+        public void AtualizarFuncionario(Funcionario Item)
+        {
+            Funcionarios.FirstOrDefault(x => x.Codigo == Item.Codigo).AtualizarFuncionario(Item);
+        }
+        public void AtualizarProduto(Produto Item)
+        {
+            Produtos.FirstOrDefault(x => x.Codigo == Item.Codigo).AtualizarProduto(Item);
+        }
+        public void AtualizarCliente(Cliente Item)
+        {
+            Clientes.FirstOrDefault(x => x.Codigo == Item.Codigo).AtualizarCliente(Item);
+        }
+        public void AtualizarVenda(Venda Item)
+        {
+            Vendas.FirstOrDefault(x => x.Codigo == Item.Codigo).AtualizarVenda(Item);
+        }
+
+        public void SalvarFuncionarios(List<Funcionario> Item)
+        {
+            SalvarEEscrever.SaveBinFile<List<Funcionario>>("funcionarios", Item);
+        }
+        public void SalvarProdutos(List<Produto> Item)
+        {
+            SalvarEEscrever.SaveBinFile<List<Produto>>("Produtos", Item);
+        }
+        public void SalvarClientes(List<Cliente> Item)
+        {
+            SalvarEEscrever.SaveBinFile<List<Cliente>>("Clientes", Item);
+        }
+        public void SalvarVendas(List<Venda> Item)
+        {
+            SalvarEEscrever.SaveBinFile<List<Venda>>("Vendas", Item);
+        }
+
+        public List<Funcionario> CarregarFuncionarios()
+        {
+            return SalvarEEscrever.ReadBinFile<List<Funcionario>>("Funcionario");
+        }
+        public List<Produto> CarregarProdutos()
+        {
+            return SalvarEEscrever.ReadBinFile<List<Produto>>("Produtos");
+        }
+        public List<Cliente> CarregarClientes()
+        {
+            return SalvarEEscrever.ReadBinFile<List<Cliente>>("Clientes");
+        }
+        public List<Venda> CarregarVendas()
+        {
+            return SalvarEEscrever.ReadBinFile<List<Venda>>("Vendas");
+        }
+    }
+}
---
+++ PadariaEMerceariaDaFah/PadariaEMerceariaDaFah/SalvarEEscrever.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Arquivos
+{   
+    [Serializable]
+    class SalvarEEscrever
+    {
+        static public List<string> ReadTextFile(string path)
+        {
+            List<string> contents = new List<string>();
+
+            //Forma 1
+            try
+            {
+                contents = File.ReadAllLines(path).ToList();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error:" + ex.Message);
+                throw ex;
+            }
+            //--//
+
+            return contents;
+            //Forma 2
+            StreamReader fs = null;
+            try
+            {
+                fs = new StreamReader(path);
+                string currentLine = "";
+                while ((currentLine = fs.ReadLine()) != null)
+                {
+                    contents.Add(currentLine);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error:" + ex.Message);
+                throw ex;
+            }
+            finally
+            {
+                fs.Close();
+            }
+            //--//
+            
+        }
+
+        static public void SaveTextFile(string path, List<string> content, FileMode Type )
+        {
+            try
+            {
+                if (Type == FileMode.Append)
+                    File.AppendAllLines(path, content);
+                else
+                    File.WriteAllLines(path, content);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw ex;                               
+            }
+       }
+
+        static public void SaveBinFile<T>(string path, T content)
+        {
+            FileStream fs = null;
+            try
+            {
+                fs = new FileStream(path, FileMode.Create, FileAccess.ReadWrite);
+
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.Serialize(fs, content);
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw (ex);
+
+            }
+            finally
+            {
+                if(fs != null)
+                    fs.Close();
+            }
+        }
+
+        static public T ReadBinFile<T>(string path)
+        {
+            FileStream fs = null;
+            T content;
+            try
+            {
+                fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+                BinaryFormatter bf = new BinaryFormatter();
+                content= (T)bf.Deserialize(fs);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw ex;
+            }
+            finally
+            {
+                fs.Close();
+            }
+
+            return content;
+        }
+
+    }
+}
