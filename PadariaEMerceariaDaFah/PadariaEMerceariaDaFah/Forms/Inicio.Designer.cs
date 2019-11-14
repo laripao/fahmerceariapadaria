833227a0caa433bb8ad2e95e7c2f63f8c8b452fe
LARISSÃO <francyelid.lari@gmail.com>
2019-11-14 10:03:11 -0300

    Correcao classe BD

--- PadariaEMerceariaDaFah/PadariaEMerceariaDaFah/Forms/Inicio.Designer.cs
+++ PadariaEMerceariaDaFah/PadariaEMerceariaDaFah/Forms/Inicio.Designer.cs
@@ -30,13 +30,19 @@
         {
             this.go_gerencia = new System.Windows.Forms.Button();
             this.go_estoque = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
             // go_gerencia
             // 
-            this.go_gerencia.Location = new System.Drawing.Point(38, 339);
+            this.go_gerencia.Location = new System.Drawing.Point(202, 15);
+            this.go_gerencia.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
             this.go_gerencia.Name = "go_gerencia";
-            this.go_gerencia.Size = new System.Drawing.Size(118, 48);
+            this.go_gerencia.Size = new System.Drawing.Size(148, 60);
             this.go_gerencia.TabIndex = 0;
             this.go_gerencia.Text = "Gerência";
             this.go_gerencia.UseVisualStyleBackColor = true;
@@ -44,24 +50,72 @@
             // 
             // go_estoque
             // 
-            this.go_estoque.Location = new System.Drawing.Point(38, 12);
+            this.go_estoque.Location = new System.Drawing.Point(48, 15);
+            this.go_estoque.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
             this.go_estoque.Name = "go_estoque";
-            this.go_estoque.Size = new System.Drawing.Size(118, 48);
+            this.go_estoque.Size = new System.Drawing.Size(148, 60);
             this.go_estoque.TabIndex = 1;
             this.go_estoque.Text = "Estoque";
             this.go_estoque.UseVisualStyleBackColor = true;
             this.go_estoque.Click += new System.EventHandler(this.go_estoque_Click);
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Font = new System.Drawing.Font("Georgia", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabControl1.Location = new System.Drawing.Point(15, 82);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(970, 465);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.tabPage1.Size = new System.Drawing.Size(962, 432);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.tabPage2.Size = new System.Drawing.Size(962, 432);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Gerência";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 29);
+            this.tabPage3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(962, 432);
+            this.tabPage3.TabIndex = 0;
+            this.tabPage3.Text = "Estoque";
+            // 
             // Inicio
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1000, 562);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.go_estoque);
             this.Controls.Add(this.go_gerencia);
+            this.Font = new System.Drawing.Font("Georgia", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
             this.Name = "Inicio";
             this.Text = "Inicio";
             this.Load += new System.EventHandler(this.Inicio_Load);
+            this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -70,6 +124,10 @@
 
         private System.Windows.Forms.Button go_gerencia;
         private System.Windows.Forms.Button go_estoque;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
     }
 }
 
