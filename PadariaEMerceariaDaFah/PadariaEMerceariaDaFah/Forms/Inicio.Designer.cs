a3f81417e891478ea96d37980ebb004031cbc244
LARISSÃO <francyelid.lari@gmail.com>
2019-11-19 23:07:21 -0300

    Correção do menu

--- PadariaEMerceariaDaFah/PadariaEMerceariaDaFah/Forms/Inicio.Designer.cs
+++ PadariaEMerceariaDaFah/PadariaEMerceariaDaFah/Forms/Inicio.Designer.cs
@@ -36,22 +36,15 @@
             this.tabEstoque = new System.Windows.Forms.TabPage();
             this.panel_produto = new System.Windows.Forms.Panel();
             this.tabGerencia = new System.Windows.Forms.TabPage();
-            this.TabGo_Fornecedor = new System.Windows.Forms.Button();
-            this.TabGoFuncionario = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.tabEstoque = new System.Windows.Forms.TabPage();
-            this.TabShowProducts = new System.Windows.Forms.Button();
-            this.tabGerencia = new System.Windows.Forms.TabPage();
             this.TabGoCliente = new System.Windows.Forms.Button();
             this.TabGo_Fornecedor = new System.Windows.Forms.Button();
             this.TabGoFuncionario = new System.Windows.Forms.Button();
             this.tabPesquisa = new System.Windows.Forms.TabPage();
+            this.groupResultados = new System.Windows.Forms.GroupBox();
+            this.btn_pesquisar = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label2 = new System.Windows.Forms.Label();
             this.tabela = new System.Windows.Forms.ComboBox();
-            this.btn_pesquisar = new System.Windows.Forms.Button();
-            this.groupResultados = new System.Windows.Forms.GroupBox();
             this.tabControl1.SuspendLayout();
             this.tabLogo.SuspendLayout();
             this.tabEstoque.SuspendLayout();
@@ -72,7 +65,6 @@
             this.tabControl1.Location = new System.Drawing.Point(12, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(856, 637);
             this.tabControl1.Size = new System.Drawing.Size(970, 750);
             this.tabControl1.TabIndex = 0;
             // 
@@ -84,29 +76,8 @@
             this.tabLogo.Location = new System.Drawing.Point(4, 30);
             this.tabLogo.Name = "tabLogo";
             this.tabLogo.Padding = new System.Windows.Forms.Padding(3);
-            this.tabLogo.Size = new System.Drawing.Size(848, 603);
-            this.tabLogo.Size = new System.Drawing.Size(846, 598);
+            this.tabLogo.Size = new System.Drawing.Size(962, 716);
             this.tabLogo.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(2, 3);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(402, 28);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Produtos que vencerão em 5 dias";
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 21;
-            this.listBox1.Location = new System.Drawing.Point(6, 52);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(834, 319);
-            this.listBox1.TabIndex = 0;
             // 
             // label1
             // 
@@ -130,42 +101,34 @@
             // 
             // tabEstoque
             // 
-            this.tabEstoque.Controls.Add(this.TabShowProducts);
-            this.tabEstoque.Location = new System.Drawing.Point(4, 30);
             this.tabEstoque.Controls.Add(this.panel_produto);
             this.tabEstoque.Location = new System.Drawing.Point(4, 30);
             this.tabEstoque.Name = "tabEstoque";
             this.tabEstoque.Padding = new System.Windows.Forms.Padding(3);
-            this.tabEstoque.Size = new System.Drawing.Size(848, 603);
             this.tabEstoque.Size = new System.Drawing.Size(962, 716);
             this.tabEstoque.TabIndex = 1;
             this.tabEstoque.Text = "Estoque";
             this.tabEstoque.UseVisualStyleBackColor = true;
             // 
-            // TabShowProducts
+            // panel_produto
             // 
-            this.TabShowProducts.Location = new System.Drawing.Point(58, 167);
-            this.TabShowProducts.Name = "TabShowProducts";
-            this.TabShowProducts.Size = new System.Drawing.Size(145, 61);
-            this.TabShowProducts.TabIndex = 1;
-            this.TabShowProducts.Text = "Visualizar produtos";
-            this.TabShowProducts.UseVisualStyleBackColor = true;
-            this.TabShowProducts.Click += new System.EventHandler(this.TabShowProducts_Click);
+            this.panel_produto.Location = new System.Drawing.Point(0, 0);
+            this.panel_produto.Name = "panel_produto";
+            this.panel_produto.Size = new System.Drawing.Size(966, 713);
+            this.panel_produto.TabIndex = 0;
             // 
             // tabGerencia
             // 
             this.tabGerencia.Controls.Add(this.TabGoCliente);
             this.tabGerencia.Controls.Add(this.TabGo_Fornecedor);
             this.tabGerencia.Controls.Add(this.TabGoFuncionario);
-            this.tabGerencia.Location = new System.Drawing.Point(4, 26);
+            this.tabGerencia.Location = new System.Drawing.Point(4, 30);
             this.tabGerencia.Name = "tabGerencia";
-            this.tabGerencia.Size = new System.Drawing.Size(846, 598);
-            this.tabGerencia.Size = new System.Drawing.Size(848, 603);
+            this.tabGerencia.Size = new System.Drawing.Size(962, 716);
             this.tabGerencia.TabIndex = 2;
             this.tabGerencia.Text = "Gerência";
             this.tabGerencia.UseVisualStyleBackColor = true;
             // 
-            // TabGo_Fornecedor
             // TabGoCliente
             // 
             this.TabGoCliente.Location = new System.Drawing.Point(23, 138);
@@ -177,14 +140,6 @@
             this.TabGoCliente.Click += new System.EventHandler(this.TabGoCliente_Click);
             // 
             // TabGo_Fornecedor
-            // 
-            this.TabGo_Fornecedor.Location = new System.Drawing.Point(352, 138);
-            this.TabGo_Fornecedor.Name = "TabGo_Fornecedor";
-            this.TabGo_Fornecedor.Size = new System.Drawing.Size(116, 66);
-            this.TabGo_Fornecedor.TabIndex = 1;
-            this.TabGo_Fornecedor.Text = "Fornecedor";
-            this.TabGo_Fornecedor.UseVisualStyleBackColor = true;
-            this.TabGo_Fornecedor.Click += new System.EventHandler(this.TabGo_Fornecedor_Click);
             // 
             this.TabGo_Fornecedor.Location = new System.Drawing.Point(266, 138);
             this.TabGo_Fornecedor.Name = "TabGo_Fornecedor";
@@ -214,11 +169,30 @@
             this.tabPesquisa.Controls.Add(this.tabela);
             this.tabPesquisa.Location = new System.Drawing.Point(4, 30);
             this.tabPesquisa.Name = "tabPesquisa";
-            this.tabPesquisa.Size = new System.Drawing.Size(848, 603);
+            this.tabPesquisa.Size = new System.Drawing.Size(962, 716);
             this.tabPesquisa.TabIndex = 3;
             this.tabPesquisa.Text = "Pesquisa";
             this.tabPesquisa.UseVisualStyleBackColor = true;
             this.tabPesquisa.Click += new System.EventHandler(this.tabPesquisa_Click);
+            // 
+            // groupResultados
+            // 
+            this.groupResultados.Location = new System.Drawing.Point(16, 159);
+            this.groupResultados.Name = "groupResultados";
+            this.groupResultados.Size = new System.Drawing.Size(829, 100);
+            this.groupResultados.TabIndex = 6;
+            this.groupResultados.TabStop = false;
+            this.groupResultados.Text = "Resultados";
+            // 
+            // btn_pesquisar
+            // 
+            this.btn_pesquisar.Location = new System.Drawing.Point(570, 14);
+            this.btn_pesquisar.Name = "btn_pesquisar";
+            this.btn_pesquisar.Size = new System.Drawing.Size(205, 29);
+            this.btn_pesquisar.TabIndex = 6;
+            this.btn_pesquisar.Text = "Pesquisar";
+            this.btn_pesquisar.UseVisualStyleBackColor = true;
+            this.btn_pesquisar.Click += new System.EventHandler(this.btn_pesquisar_Click);
             // 
             // groupBox1
             // 
@@ -253,25 +227,6 @@
             this.tabela.Size = new System.Drawing.Size(161, 29);
             this.tabela.TabIndex = 3;
             this.tabela.SelectedIndexChanged += new System.EventHandler(this.tabela_SelectedIndexChanged);
-            // 
-            // btn_pesquisar
-            // 
-            this.btn_pesquisar.Location = new System.Drawing.Point(570, 14);
-            this.btn_pesquisar.Name = "btn_pesquisar";
-            this.btn_pesquisar.Size = new System.Drawing.Size(205, 29);
-            this.btn_pesquisar.TabIndex = 6;
-            this.btn_pesquisar.Text = "Pesquisar";
-            this.btn_pesquisar.UseVisualStyleBackColor = true;
-            this.btn_pesquisar.Click += new System.EventHandler(this.btn_pesquisar_Click);
-            // 
-            // groupResultados
-            // 
-            this.groupResultados.Location = new System.Drawing.Point(16, 159);
-            this.groupResultados.Name = "groupResultados";
-            this.groupResultados.Size = new System.Drawing.Size(829, 100);
-            this.groupResultados.TabIndex = 6;
-            this.groupResultados.TabStop = false;
-            this.groupResultados.Text = "Resultados";
             // 
             // Inicio
             // 
